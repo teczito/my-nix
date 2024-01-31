d39bd71b90c7cd07ade154b652aa2bc971c6c47c
Ruben de Schipper <rubendeschipper@gmail.com>
2024-01-31 17:09:03 +0100

    update + remove i3

--- configuration.nix
+++ configuration.nix
@@ -95,16 +95,6 @@
       };
     };
    
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
-     ];
-    };
-
     windowManager.awesome = {
       enable = true;
       luaModules = with pkgs; [
