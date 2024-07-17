44cc9a2929a28684aa8c88e96856cac146dea58c
Ruben de Schipper <rubendeschipper@gmail.com>
2024-07-17 07:32:28 +0200

    Remove sound from configuration file

--- configuration.nix
+++ configuration.nix
@@ -149,7 +149,6 @@
   services.udev.extraRules = import ./config-files/ti/71-ti-permissions.rules;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.polkit.enable = true;
