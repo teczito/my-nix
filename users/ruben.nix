6ac2a20e2a217575f0951f686dd028b0fe2560e1
Ruben de Schipper <rubendeschipper@gmail.com>
2024-11-21 13:57:49 +0100

    Update tmux conf

--- users/ruben.nix
+++ users/ruben.nix
@@ -56,8 +56,7 @@
           # easy-to-remember split pane commands
           bind | split-window -h -c "#{pane_current_path}"
           bind - split-window -v -c "#{pane_current_path}"
-          bind c new-window -c "#{pane_current_path}"
-          bind a switch-client
+          bind C-c new-window -c "#{pane_current_path}"
         '';
       };
 
